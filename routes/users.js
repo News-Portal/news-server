0a30928c410d77a556129bc43fe53b4f594a4fa4
vinesto <oqvinesto@gmail.com>
2018-09-06 19:52:15 +0700

    add login fb

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var {login,register} = require('../controllers/users')
+var { login, register, loginFb } = require('../controllers/users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login',login)
-router.post('/register',register)
+router.post('/login', login)
+router.post('/register', register)
+router.post('/loginFb',loginFb)
 
 module.exports = router;
